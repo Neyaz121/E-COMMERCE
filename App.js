27460fc4816db05e4e00b2eeaf6b51ac9a47968c
Neyaz Ahmad <neyazahmad0726@gmail.com>
2025-09-02 09:01:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import { Counter } from './Features/counter';
+import './App.css'
+import ProductList from './Features/Product-list';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <ProductList></ProductList>
+    </div>
+    
+  )
+}
+
+export default App
